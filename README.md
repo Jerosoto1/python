f768e0be609b7abcd70b95c9fdd86c9c2bb9eb35
Jerosoto1 <jerosoto2410@gmail.com>
2025-05-12 17:41:00 -0500

    Add files via upload

---
+++ README.md
@@ -0,0 +1,22 @@
+**Program Explanation**
+
+# ***Menu***
+# We provide the menu so the operations leader can select the desired option.
+
+# ***Option 1***
+# If the operations leader selects option 1, the program will add the product to the inventory and save it.
+
+# ***Option 2***
+# If the operations leader selects option 2, the program will request the name of the product they wish to search for and enter its name, price, and inventory quantity.
+
+# ***Option 3***
+# If the operations leader selects option 3, it will ask for the name of the product they wish to update the price of and save its new value to the inventory.
+
+# ***Option 4***
+# If the operations leader selects option 4, it will delete the product from the inventory by simply entering its name.
+
+# ***Option 5***
+# If the operations leader selects option 5, they will be able to see the total sum of all the product in the inventory.
+
+# ***Option 6***
+# Exit the progam
---
+++ archivoDePrueba.py
@@ -0,0 +1,132 @@
+#Test Invetory
+
+#Here we are burning the dictionary list so that it is not empty and we add some products to it.
+inventory = [
+    {"name":"nails","price":5000,"quantity":22},
+    {"name":"milk", "price":3200,"quantity":24},
+    {"name":"coffe","price":2000,"quantity":12},
+    {"name":"rice","price":2200,"quantity":46},
+    {"name":"candy","price":500,"quantity":100},
+    {"name": "oil", "price": 12000, "quantity": 12}
+]
+
+
+
+#Here we are giving you a function so that when you enter numbers that are not valid, it asks you to enter a negative number.
+def input_num(prompt, tipo=float):
+    while True:
+        entrance = input(prompt)
+        try:
+            valor = tipo(entrance)
+            if valor <= 0:
+                print("Must be a positive number!.")
+                continue
+            return valor
+        except ValueError:
+            print("Invalid entry. Please enter a valid number.")
+
+
+#Here we create a function so that you can add product and in case you want to enter a product that already exists, it will tell you a message that the product already exists.
+def add_product(name, price, quantity):
+    for product in inventory:
+        if product["name"].lower() == name.lower():  
+            print("The product already exists in the inventory.")
+            return
+    inventory.append({"name": name, "price": round(price, 2), "quantity": int(quantity)})
+    print(f"Product '{name}' successfully added with price ${price:.2f} and quantity {quantity}.")
+
+
+#Here we make a new function so that the user can check if the product is available or, if so, print a message that it is not found.
+def search_product(name):
+    for product in inventory:
+        if product["name"].lower() == name.lower():  
+            print(f"{product['name']} | Price: ${product['price']:.2f} | Quantity: {product['quantity']}")
+            return
+    print("product not found.")
+
+
+#Here by performing this function we can easily update the price of the product.
+def update_price(name, new_price):
+    for product in inventory:
+        if product["name"].lower() == name.lower():
+            product["price"] = round(new_price, 2)
+            print(f"Price of '{name}' updated to ${new_price:.2f}.")
+            return
+    print("product not found.")
+
+#Here we perform a function so that products can be easily removed from the inventory.
+def del_product(name):
+    for product in inventory:
+        if product["name"].lower() == name.lower():
+            inventory.remove(product)
+            print(f"The Product '{name}' was removed from inventory.")
+            return
+    print("Product not found.")
+
+#Here with this function what we do is calculate the total value of the inventory
+def total_inventory_value():
+    total = sum(product["price"] * product["quantity"] for product in inventory)
+    print(f"Total inventory value: ${total:.2f}")
+
+
+#Here we create the menu with a function so that when you need to call it, it can be done much faster.
+def menu ():
+    while True: 
+        print ("\n*** Menu ***\n")
+        print ("1. Add product")
+        print ("2. Search product")
+        print ("3. Update price")
+        print ("4. Delete product from inventory")
+        print ("5. Calculate the price of inventory")
+        print ("\n6. Exit-> \n")
+        
+        option = input("Choose an option (1-5): ")
+
+
+
+        #Here we call the function and ask it for the requirements to add the product
+        if option == "1":
+            name = input("\nProduct title: ").strip()
+            price = input_num("Product price: ")
+            quantity = input_num("Quantity: ", int)
+            add_product(name, price, quantity)
+
+
+
+        #Here we just tell it to call the function and give the name of the product to search for.
+        elif option == "2":
+            name = input ("Give me the name of the product to search for it: ")
+            search_product(name)
+
+
+
+        #Here we ask the user to enter the name of the product to update its price
+        elif option == "3":
+            name = input("product title to update: ").strip()
+            new_price = input_num("New price: ")
+            update_price(name, new_price)
+
+
+
+        #Here we ask for the name of the peoduct to remove it from the inventory
+        elif option == "4":
+            name = input ("product title to delete: ")
+            del_product(name)
+
+
+
+        #Here we only call the function to know how much the entire inventory is worth.
+        elif option == "5":
+            total_inventory_value()
+
+
+
+        #Here we simply break the program and get out of it
+        elif option == "6":
+            print ("\n ** Exiting program... ** \n")
+            break 
+        else:
+            print ("\nInvalid option. Please choose between 1 and 6\n")
+
+if __name__ == "__main__":
+    menu()
