69079d20d5080e59870212c924ad0ca15cfbaa59
Jerosoto1 <jerosoto2410@gmail.com>
2025-05-13 21:28:57 -0500

    Add files via upload

---
+++ Ejercicios.py
@@ -0,0 +1,21 @@
+##EJERCICIOS 1. Level Basic.
+# # 1. Saludo personalizado
+# def saludar(nombre):
+#     mensaje = f"Que mas?, {nombre}"
+#     return mensaje
+# nombre = input("Cual es tu nombre?: ")
+# print(saludar (nombre))
+
+# 2. Suma de dos números
+flag = True
+def sumar(a, b):
+    return a + b
+
+numero1 = float(input("Ingrese el primer numero: "))
+numero2 = float(input("Ingrese el segundo numero: "))
+print(numero1+numero2)
+
+
+
+
+# 3. Número par o impar
